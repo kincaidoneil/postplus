181cb4cf019a9e62a3266e4f0693bac14b8d605f
Kincaid O'Neil <kincaidoneil@gmail.com>
2016-06-05 13:53:35 -0500

    Bundle Electron main/background process
    - Add an additional entry point so electron.js is also bundled
    separately
    - Use path.join instead of relative paths because why not
    - Use hashes instead of filenames for assets
    - Increase the max size of inlined assets to 128KB
    - Set target to 'electron-render' so Node.js modules are bundled (or so
    I think?)
    - Move .babelrc presets/plugins to webpack config instead of separate
    file
    - Remove unnecessary Webpack plugins
    - Only inject electron renderer process (index.js) into generated HTML
    file
    - Set 'dirname' to '__dirname' in the bundled files because Webpack
    screws up the __dirname global so its a relative path instead of
    absolute (AHHH)
    - Add source maps support
    - Update Webpack config with ES6 since I updated Node to 6.x
    - Use camelCase instead of underscores
    - Add trash-cli command to NPM scripts to clean dist/ directory before
    each new Webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
-var webpack = require('webpack');
-var html_webpack_plugin = require('html-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const htmlPlugin = require('html-webpack-plugin')
 
-var options = {
-	entry: ['./app/index.js'],
+let options = {
+	entry: {
+		electron: path.join(__dirname, 'app/electron.js'),
+		index: path.join(__dirname, 'app/index.js')
+	},
 	output: {
-		path: './dist',
-		publicPath: 'http://localhost:8080/dist',
-		filename: 'build.js'
+		path: path.join(__dirname, 'dist/'),
+		filename: '[name].js'
 	},
 	module: {
 		loaders: [{
@@ -20,10 +23,10 @@ var options = {
 			test: /\.(jpe?g|png|gif|svg|ttf)/,
 			loader: 'url',
 			query: {
-				// Limit the size of inline files to 10KB
-				limit: 10000,
+				// Limit the size of inline files to 128KB
+				limit: 128000,
 				// Fallback to file-loader with this naming scheme
-				name: '[name].[ext]?[hash]'
+				name: '[hash:12].[ext]'
 			}
 		}, {
 			test: /\.styl$/,
@@ -33,25 +36,42 @@ var options = {
 			loader: 'style-loader!css-loader'
 		}]
 	},
+	// Bundle all Node.js modules (I think this works...?)
+	target: 'electron-renderer',
 	resolve: {
 		modulesDirectories: ['node_modules', 'app/modules']
 	},
+	babel: {
+		"presets": ["es2015"],
+		"plugins": ["transform-runtime"]
+	},
 	plugins: [
-		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.NoErrorsPlugin(),
-		new html_webpack_plugin({
+		// Generate an HTML file using the template and inject scripts
+		new htmlPlugin({
 			filename: 'index.html',
 			template: 'app/index.html',
-			inject: true
+			inject: true,
+			// Only include the render process (index.js) in the HTML file
+			// Exclude the Electron main process
+			chunks: ['index']
+		}),
+		// Webpack fucks up __dirname in electron.js, so save it to dirname instead
+		// Basically, __dirname would get set to '/' instead of the full path from 'C:/'
+		// More info here: https://github.com/webpack/webpack/issues/1599
+		new webpack.DefinePlugin({
+			dirname: '__dirname',
 		})
 	]
-};
+}
 
 if (process.env.NODE_ENV === 'production') {
 	// Minify the output
-	options.plugins.push(new webpack.optimize.UglifyJsPlugin());
+	options.plugins.push(new webpack.optimize.UglifyJsPlugin())
 	// Omit localhost during production (used for hot reloading)
-	options.output.publicPath = './';
+	options.output.publicPath = './'
+} else {
+	options.devtool = '#inline-source-map'
+	options.publicPath = 'http://localhost:8080/dist'
 }
 
-module.exports = options;
+module.exports = options
